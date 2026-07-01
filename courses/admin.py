57c139c9efcac3a6b1a5a1ab09a88825365c69ff
AmruthaSachin <amrutha.nagvekar@gmail.com>
2026-07-01 16:08:52 +0530

    Remove old Online Learning project

--- courses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Course, CourseContent
-from .models import Course, CourseContent, Enrollment
-
-admin.site.register(Enrollment)
-admin.site.register(Course)
-admin.site.register(CourseContent)
